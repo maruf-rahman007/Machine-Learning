ccd74403b84576ecf5c6ed5ea7733542dacaa18c
maruf-rahman007 <rahman15-6212@s.diu.edu.bd>
2025-01-09 20:54:16 +0600

    first repor setup

---
+++ Python/Basics/calculator.py
@@ -0,0 +1,7 @@
+def single_Sum():
+    a = input("First value : ")
+
+
+
+print("Welcome To Apple Calculator")
+operations = input("Choose operations \nPress 1 for Sum\nPress 2 for Subtraction\nPress 3 for Multiplication")
---
+++ Python/Basics/hello.py
@@ -0,0 +1,7 @@
+first_name = "Maruf"
+last_name = "Rahman"
+uni_name = "Daffodil International University"
+uni_course = "Computer Science"
+uni_year = 3
+
+print(f"Hello everyone I'm {first_name} {last_name} a {uni_year}rd year {uni_course} student from {uni_name} department of {uni_course} & Engineering.")
